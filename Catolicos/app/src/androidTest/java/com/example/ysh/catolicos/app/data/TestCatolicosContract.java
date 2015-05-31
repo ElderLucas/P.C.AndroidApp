910f27eeab231d1d46ecf5946bad8f9ec8709f41
Elder Lucas <elderlucas@gmail.com>
2015-05-31 12:30:46 -0300

    Adicionado o teste do Catolicos Contrato

---
+++ Catolicos/app/src/androidTest/java/com/example/ysh/catolicos/app/data/TestCatolicosContract.java
@@ -0,0 +1,27 @@
+package com.example.ysh.catolicos.app.data;
+
+import android.net.Uri;
+import android.test.AndroidTestCase;
+
+
+/*
+  Created by YSH on 31/05/2015.
+  Testar o contract
+ */
+public class TestCatolicosContract extends AndroidTestCase {
+
+    // intentionally includes a slash to make sure Uri is getting quoted correctly
+    static final String TEST_ACTIVITY_PARISH = "SaoDimas";
+    public void testBuildActivityParishURi() {
+
+        Uri ActivityParishUri = CatolicosContract.ActivityEntry.build_ActivityParish(TEST_ACTIVITY_PARISH);
+
+        assertNotNull("Error: Null Uri returned.  You must fill-in buildWeatherLocation in " + "CatolicosContract.", ActivityParishUri);
+
+        assertEquals("Error: Weather location not properly appended to the end of the Uri", TEST_ACTIVITY_PARISH, ActivityParishUri.getLastPathSegment());
+
+        assertEquals("Error: Weather location Uri doesn't match our expected result", ActivityParishUri.toString(), "content://com.example.ysh.catolicos.app/atividade/SaoDimas");
+    }
+
+
+}
